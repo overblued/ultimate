bbfcb44c39c5f6eaa674830312cae67568e6b174
Bangyi JIANG <ace.jby@gmail.com>
2014-02-05 23:46:24 +0800

    new effect for nav button

--- html/js/intro.js
+++ html/js/intro.js
@@ -4,9 +4,9 @@
 	var showcase = $(document.createElement('ul')).set({ className: 'showcase', }),
 		template = '<h4>{{name}}</h4><p>{{des}}</p>',
 		
-		nav = document.getElementsByClassName('nav')[0],
-		menu = nav.children[0],
-		currentLabel = nav.children[1],
+		menu = $('#nav ul'),
+		btn1 = $('#nav ul li:nth-child(1) a'),
+		btn2 = $('#nav ul li:nth-child(2) a'),
 	
 		app = {
 			name: "Menu",
@@ -17,9 +17,26 @@
 					showcase.appendTo(theProject.stage);
 				})();
 			}
-		};
-		
-	menu.onclick = function (){ switchApp(app.name) };
+		};console.log(btn2);
+	//scroll effect
+	menu.set({
+		onmouseover: function (){
+			if (theProject.current !== app){
+				this.styles({marginTop: 0});
+			}
+		},
+		onmouseout: function (){
+			if (theProject.current !== app)
+			this.styles({marginTop: '-2em'});
+		}
+	});
+	
+	//make it a home button
+	btn1.set({
+		innerText: app.name,
+		onclick: function (){ switchApp(app.name); }
+	});
+	
 	
 	function newApp(name){
 		var newOne,
@@ -37,10 +54,15 @@
 	
 	//take the name of an app, launch it.
 	function switchApp(name){
-		if (theProject.launch(name))
-			currentLabel.innerText = name;
+		if (theProject.launch(name)){
+			menuText(name);
+			if (name !== app.name)
+				menu.styles({marginTop: '-2em'});
+		}
 	}
-	
+	function menuText(txt){
+		btn2.set({innerText: txt});
+	}
 	theProject.new(app);
 	//launche this immediatly after loading;
 	switchApp(app.name);
